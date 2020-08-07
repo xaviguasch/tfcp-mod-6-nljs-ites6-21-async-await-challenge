b9931d4ffe227ec4de57311b61fe49e3185efa05
xaviguasch <xaviguasch@gmail.com>
2020-08-07 19:45:32 +0200

    added starter files

---
+++ index.js
@@ -0,0 +1,5 @@
+//Challenge - Async & Await
+
+//Print on the console a random joke from the Chuck Norris API using Fetch and Async and Await
+
+const apiUrl = 'https://api.chucknorris.io/jokes/random'
