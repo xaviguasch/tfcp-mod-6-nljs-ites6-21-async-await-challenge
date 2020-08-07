12315830e5971f5c6908010204e5607b4d0a1209
xaviguasch <xaviguasch@gmail.com>
2020-08-07 19:45:55 +0200

    added solution

--- index.js
+++ index.js
@@ -3,3 +3,12 @@
 //Print on the console a random joke from the Chuck Norris API using Fetch and Async and Await
 
 const apiUrl = 'https://api.chucknorris.io/jokes/random'
+
+const printRandom = async () => {
+  let call = await fetch(apiUrl)
+  let response = await call.json()
+
+  console.log(response.value)
+}
+
+printRandom()
